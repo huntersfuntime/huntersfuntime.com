ba20279de7b9928ad52b630859c898144797cdbc
hunterorges <hunterorges@gmail.com>
2018-07-11 16:03:26 -0600

    Created concern for placeholder image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,17 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     # def self.angular
     #     where(subtitle: 'Angular')
     # end
     # # both are scopes
-    # def scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+    # def scope :ruby_on_rails, ~> { where(subtitle: 'Ruby on Rails') }
 
     after_initialize :set_defaults
 
     def set_defaults 
-        self.main_image ||= "http://via.placeholder.com/600x200"
-        self.thumb_image ||= "http://via.placeholder.com/350x250"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 end
